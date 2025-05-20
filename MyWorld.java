644340f72292bebcab05f4d25a7dddd5b7c36fec
marin-yan <marinxyan@gmail.com>
2025-05-20 08:42:57 -0400

    added the capybara sprite to the world sucessfully

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,7 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        Capybara capybara = new Capybara();
+        addObject(capybara, 300, 300);
     }
 }
