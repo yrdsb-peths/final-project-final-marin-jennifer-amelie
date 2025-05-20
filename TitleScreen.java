7b221916129d241cf39b416e0ec943a27123b964
JenniferTuWu <jenniferydt@gmail.com>
2025-05-20 08:45:51 -0400

    Update TitleScreen.java

--- TitleScreen.java
+++ TitleScreen.java
@@ -15,8 +15,9 @@ public class TitleScreen extends World
         GreenfootImage titleBg = new GreenfootImage("images/titleBg.jpg");
         getBackground().drawImage(titleBg, 275, 100);
         addObject(titleLabel, 275, 100);
+        prepare();
     }
-    
+
     /**
      * The main world act loop
      */
@@ -25,5 +26,20 @@ public class TitleScreen extends World
             MyWorld gameWorld = new MyWorld();
             Greenfoot.setWorld(gameWorld);
         }
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Label label = new Label("Press <space> to Start", 20);
+        addObject(label,91,191);
+        label.setLocation(138,177);
+        
+        Label label2 = new Label("Use \u21D0 and \u21D2 to Move", 20);
+        addObject(label2,56,222);
+        label2.setLocation(134,214);
     }
 }
