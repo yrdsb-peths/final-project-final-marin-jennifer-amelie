e4342d96dad37fa30bc0375772f3401e4826bb51
marin-yan <marinxyan@gmail.com>
2025-05-21 08:13:33 -0400

    added the idle animation to capybara

--- Capybara.java
+++ Capybara.java
@@ -12,8 +12,10 @@ public class Capybara extends Actor
      * Act - do whatever the Capybara wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    GreenfootImage[] idleRight = new GreenfootImage[5];
-    GreenfootImage[] idleLeft = new GreenfootImage[5];
+    GreenfootImage[] idleRight = new GreenfootImage[8];
+    GreenfootImage[] idleLeft = new GreenfootImage[8];
+    GreenfootImage[] walkRight = new GreenfootImage[5];
+    GreenfootImage[] walkLeft = new GreenfootImage[5];
     //direction of capybara
     String facing = "right";
     SimpleTimer animationTimer = new SimpleTimer();
@@ -23,22 +25,36 @@ public class Capybara extends Actor
      */
     
     public Capybara() {
+        // idle animation
         for(int i = 0; i<idleRight.length; i++){
-            idleRight[i] = new GreenfootImage("images/walking_capybara/walking00"+i+".png");
+            idleRight[i] = new GreenfootImage("images/idle_capybara/idle00"+i+".png");
             idleRight[i].mirrorHorizontally();
-            idleRight[i].scale(120, 100);
+            idleRight[i].scale(120, 120);
         }
         for(int i = 0; i<idleLeft.length; i++){
-            idleLeft[i] = new GreenfootImage("images/walking_capybara/walking00"+i+".png");
-            idleLeft[i].scale(120, 100);
+            idleLeft[i] = new GreenfootImage("images/idle_capybara/idle00"+i+".png");
+            idleLeft[i].scale(120, 120);
+        }
+        
+        // walking animation
+        for(int i = 0; i<walkRight.length; i++){
+            walkRight[i] = new GreenfootImage("images/walking_capybara/walking00"+i+".png");
+            walkRight[i].mirrorHorizontally();
+            walkRight[i].scale(120, 100);
+        }
+        for(int i = 0; i<walkLeft.length; i++){
+            walkLeft[i] = new GreenfootImage("images/walking_capybara/walking00"+i+".png");
+            walkLeft[i].scale(120, 100);
         }
         animationTimer.mark();
         setImage(idleRight[0]);
+        setImage(walkRight[0]);
     }
     int imageIndex = 0;
     
-    // animate capybara
-    public void animateCapybara(){
+    // capybara animations
+    //idle
+    public void idleCapybara(){
         if(animationTimer.millisElapsed() < 100){
             return;
         }
@@ -52,18 +68,35 @@ public class Capybara extends Actor
         }
         
     }
+    // walking
+    public void walkingCapybara(){
+        if(animationTimer.millisElapsed() < 100){
+            return;
+        }
+        animationTimer.mark();
+        if(facing.equals("right")){
+            setImage(walkRight[imageIndex]);
+            imageIndex = (imageIndex + 1) % walkRight.length;  
+        } else {
+            setImage(walkLeft[imageIndex]);
+            imageIndex = (imageIndex + 1) % walkLeft.length;
+        }
+        
+    }
     
     public void act()
     {
         if(Greenfoot.isKeyDown("left")){            
             move(-5);
             facing = "left";
+            walkingCapybara();
         }
         if(Greenfoot.isKeyDown("right")){
             move(5);
             facing = "right";
+            walkingCapybara();
         }
-        animateCapybara();
+        idleCapybara();
     }
     
     
