c57ae8b713c32166c365f769693dd612abdc0bb5
JenniferTuWu <jenniferydt@gmail.com>
2025-05-21 08:28:38 -0400

    Update MyWorld.java

--- MyWorld.java
+++ MyWorld.java
@@ -4,7 +4,7 @@ public class MyWorld extends World {
     public MyWorld() {
         super(680, 520, 1);
         Capybara capybara = new Capybara();
-        addObject(capybara, 300, 300);
+        addObject(capybara, 60, 450);
         
         setBackground("images/gameBG.png");
     }
