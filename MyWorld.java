c52b033b4e3f0dbab1310ad0b9f597808fde9081
JenniferTuWu <jenniferydt@gmail.com>
2025-05-22 09:24:38 -0400

    Counting

--- MyWorld.java
+++ MyWorld.java
@@ -1,6 +1,11 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
+    SimpleTimer tim = new SimpleTimer();
+    Counter timeCount = new Counter();
+    boolean time = true;
+    int start = 0;
+    
     public MyWorld() {
         super(680, 515, 1);
         Capybara capybara = new Capybara();
@@ -10,5 +15,22 @@ public class MyWorld extends World {
         //Log log = new Log();
         //addObject(log, 0, 0);
         setBackground("images/gameBG.png");
+        
+        addObject(timeCount, 340, 20);
+        timeCount.setValue(90);
+    }
+    
+    public void act(){
+        if (start == 1){
+            if (tim.millisElapsed() > 1000){
+                timeCount.add(-1);
+                tim.mark();
+            }
+        }
+        
+        if (Greenfoot.isKeyDown("right")){
+            start = 1;
+            tim.mark();
+        }
     }
 }
