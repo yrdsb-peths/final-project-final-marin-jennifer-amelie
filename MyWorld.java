2bad804e5865110a636f3a9ac7234cea6be61ee5
amethyst2093 <wonton0409@gmail.com>
2025-05-22 09:25:25 -0400

    start log + water/poison

--- MyWorld.java
+++ MyWorld.java
@@ -2,13 +2,18 @@ import greenfoot.*;
 
 public class MyWorld extends World {
     public MyWorld() {
-        super(680, 520, 1);
+        super(680, 520, 1, false);
+        setBackground("images/gameBG.png");
         Capybara capybara = new Capybara();
         addObject(capybara, 60, 475);
-        Log floor = new Log();
-        addObject(floor, 340, 510);
-        Log log = new Log();
-        addObject(log, 0, 0);
-        setBackground("images/gameBG.png");
+        //adding platforms
+        Log floor1 = new Log("long");
+        addObject(floor1, 0, 515);
+        Log floor2 = new Log("short");
+        addObject(floor2, 430, 515);
+        Log floor3 = new Log("short");
+        addObject(floor3, 570, 515);
+        Log floor4 = new Log("stump");
+        addObject(floor3, 570, 515);
     }
 }
