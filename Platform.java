0a50471649594df4f499e161eabb01509cad5567
marin-yan <marinxyan@gmail.com>
2025-05-22 09:11:36 -0400

    adjusted the size of the capybara idle images

--- Platform.java
+++ Platform.java
@@ -12,6 +12,11 @@ public class Platform extends Actor
      * Act - do whatever the Platform wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
+    public Platform() {
+        setImage("images/treetrunks/wood_plank.png");  
+    }
+    
     public void act()
     {
         // Add your action code here.
