f9b82cbf43378c9af3627e4b9d6c6fb767cf2f20
marin-yan <marinxyan@gmail.com>
2025-05-23 15:23:44 -0400

    Update Water.java

--- Water.java
+++ Water.java
@@ -11,27 +11,26 @@ public class Water extends Actor
     GreenfootImage[] waves = new GreenfootImage[63];
     SimpleTimer animationTimer = new SimpleTimer();
     
-    public Water() {
-        for(int i = 0; i<waves.length; i++){
-            waves[i] = new GreenfootImage("images/waterAnimation/waterAnimation"+(i+1)+".png");
-            waves[i].scale(90, 75);
-        }
-        animationTimer.mark();
-        setImage(waves[0]);
-    }
+    //public Water() {
+        //for(int i = 0; i<waves.length; i++){
+           // waves[i] = new GreenfootImage("images/waterAnimation/waterAnimation"+(i+1)+".png");
+         //   waves[i].scale(90, 75);
+       // }
+     //   animationTimer.mark();
+    //    setImage(waves[0]);
+    //}
     
-    int waveIndex = 0;
+    //int waveIndex = 0;
     
-    public void waving() {
-        if(animationTimer.millisElapsed() < 100){
-            return;
-        }
-        animationTimer.mark();
-        setImage(waves[waveIndex]);
-        waveIndex = (waveIndex + 1) % waves.length;
-    }
-    public void act()
-    {
-        waving();
-    }
+    //public void waving() {
+     //   if(animationTimer.millisElapsed() < 100){
+     //       return;
+      //  }
+      //  animationTimer.mark();
+//        setImage(waves[waveIndex]);
+      //  waveIndex = (waveIndex + 1) % waves.length;
+    //}
+    //public void act()
+    //{
+    //}
 }
