3543453fdd9b740463826914bf64691084fc83d1
amethyst2093 <wonton0409@gmail.com>
2025-05-27 08:58:07 -0400

    high last1

--- MyWorld.java
+++ MyWorld.java
@@ -34,6 +34,6 @@ public class MyWorld extends World {
         Log last = new Log("short");
         addObject(last, 200, 95);
         Log last1 = new Log("long");
-        addObject(last1, 600, 130);
+        addObject(last1, 600, 95);
     }
 }
