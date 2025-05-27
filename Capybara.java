8b5b146a8a8b9d220a1aafdb09e7885903946a95
marin-yan <marinxyan@gmail.com>
2025-05-27 09:09:46 -0400

    stops capybara from moving after it touches the toxin

--- Capybara.java
+++ Capybara.java
@@ -119,6 +119,11 @@ public class Capybara extends Actor
     
     public void act()
     {
+        MyWorld world = (MyWorld) getWorld();
+        if (world.isGameOver()) {
+            return;
+        }
+        
         checkFall();
         if(Greenfoot.isKeyDown("left")){            
             move(-7);
