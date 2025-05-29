3a6133a1766b3c83db282c7dbb7f88154c339f9a
amethyst2093 <wonton0409@gmail.com>
2025-05-29 09:00:57 -0400

    switch class

---
+++ Switch.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Switch here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Switch extends Actor
+{
+    public Switch(){
+        GreenfootImage Lswitch = new GreenfootImage("images/
+    }
+    /**
+     * Act - do whatever the Switch wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
