e53001ea56ddce6f6ba8ddadd1f4f75a56042695
marin-yan <marinxyan@gmail.com>
2025-06-03 09:24:53 -0400

    updated scene1

--- Scene2.java
+++ Scene2.java
@@ -9,11 +9,13 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Scene2 extends MainWorld
 {
 
-    /**
-     * Constructor for objects of class Scene2.
-     * 
-     */
+    Label label1 = new Label("One day, when Capybara and Birdie are walking in their garden", 30);
+    
     public Scene2()
     {
+        super(680, 520, 1);
+        GreenfootImage kingdomBg = new GreenfootImage("images/garden.png");
+        kingdomBg.scale(680, 520); 
+        getBackground().drawImage(kingdomBg, 0, 0);
     }
 }
