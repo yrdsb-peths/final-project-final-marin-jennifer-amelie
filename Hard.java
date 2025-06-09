209e8e944a43261100ab70428cf5011b96968d88
amethyst2093 <wonton0409@gmail.com>
2025-06-09 09:19:13 -0400

    coins

--- Hard.java
+++ Hard.java
@@ -2,7 +2,11 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Hard extends MainWorld
 {
-
+    private boolean gameOver = false;
+    
+    //changable for timer here
+    int timer = 3170;
+    GreenfootSound bg;
     /**
      * Constructor for objects of class Hard.
      * 
@@ -10,7 +14,7 @@ public class Hard extends MainWorld
     public Hard()
     {    
         super(700, 600, 1);
-        
+        bg = new GreenfootSound("jungle.mp3");
         setBackground("levels/hard.png");
         Capybara capybara = new Capybara();
         addObject(capybara, 150, 100);
@@ -46,22 +50,56 @@ public class Hard extends MainWorld
         
         //portal
         Portal portal = new Portal();
-        addObject(portal, 250, 225);
+        addObject(portal, 300, 225);
         
+        //coins
+        Coin coin = new Coin();
+        addObject(coin, 100, 540);
+        Coin coin1 = new Coin();
+        addObject(coin1, 50, 450);
+        Coin coin2 = new Coin();
+        addObject(coin2, 515, 540);
+        Coin coin3 = new Coin();
+        addObject(coin3, 550, 125);
+        Coin coin4 = new Coin();
+        addObject(coin4, 230, 300);
+        //add timer 
+        Timer t = new Timer();
+        addObject(t, 342, 20);
         // toxin
-        Toxin toxin = new Toxin(80, 15);
-        addObject(toxin, 263, 150);
+        Toxin toxin = new Toxin(85, 10);
+        addObject(toxin, 262, 155);
         
-        Toxin toxin2 = new Toxin(155, 15);
-        addObject(toxin2, 225, 580);
+        Toxin toxin2 = new Toxin(160, 20);
+        addObject(toxin2, 225, 590);
         
-        Toxin toxin3 = new Toxin(80, 15);
-        addObject(toxin3, 430, 580);
+        Toxin toxin3 = new Toxin(80, 20);
+        addObject(toxin3, 430, 590);
+        
+        Toxin toxin4 = new Toxin(150, 20);
+        addObject(toxin4, 633, 590);
+        
+        Toxin toxin5 = new Toxin(60, 20);
+        addObject(toxin5, 25, 590);
     }
     public void act(){
         if(Log.getYes()){
             Log temp = new Log("short");
-            addObject(temp, 250, 275);
+            addObject(temp, 225, 275);
+        }
+        
+        //add number on timer
+        timer--;
+        showText(""+timer / 105, 342, 20);
+
+        if (timer <= 0){
+            setGameOver(true);
+            bg.stop();
+            GreenfootImage gameOverImage = new GreenfootImage("Game Over", 60, Color.RED, Color.BLACK);
+            addObject(new GameOverLabel(gameOverImage), getWidth() / 2, getHeight() / 2);
+            
+            addObject(new RestartButton(), getWidth() / 2 - 100, getHeight() / 2 + 80);
+            addObject(new MenuButton(), getWidth() / 2 + 100, getHeight() / 2 + 80);
         }
     }
 }
