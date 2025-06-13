d197202cc15cab7ef42f03f5d7ac719afda5cb57
marin-yan <marinxyan@gmail.com>
2025-06-13 08:49:56 -0400

    fixed the gif error in scene 3

--- Rainybara.java
+++ Rainybara.java
@@ -11,23 +11,23 @@ public class Rainybara extends Actor
     SimpleTimer animationTimer = new SimpleTimer();
     
     public Rainybara() {
-        //for(int i = 0; i<rainybara.length; i++){
-        //    rainybara[i] = new GreenfootImage("images/rainybara/frame_"+i+"_delay-0.1s.gif");
-        //    rainybara[i].scale(150, 150);
-        //}
-        GreenfootImage rainybara = new GreenfootImage("images/rainybara/frame_7_delay-0.1s.gif");
+        for(int i = 0; i<rainybara.length; i++){
+            rainybara[i] = new GreenfootImage("images/rainybara/frame_"+i+"_delay-0.1s.png");
+            rainybara[i].scale(150, 150);
+        }
+        GreenfootImage rainybara = new GreenfootImage("images/rainybara/frame_7_delay-0.1s.png");
         rainybara.scale(100, 100);
         setImage(rainybara);
     }
     
     int rainybaraIndex = 0;
     // narration
-    //public void rainybara () {
-      //  if(animationTimer.millisElapsed() < 100){
-        //    return;
-        //}
-        //animationTimer.mark();
-        //setImage(rainybara[rainybaraIndex]);
-        //rainybaraIndex = (rainybaraIndex + 1) % rainybara.length;
-    //}
+    public void rainybara () {
+        if(animationTimer.millisElapsed() < 100){
+            return;
+        }
+        animationTimer.mark();
+        setImage(rainybara[rainybaraIndex]);
+        rainybaraIndex = (rainybaraIndex + 1) % rainybara.length;
+    }
 }
