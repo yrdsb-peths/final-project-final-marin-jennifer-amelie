0136e50df9e2bd2c50854153f69572340d961924
JenniferTuWu <jenniferydt@gmail.com>
2025-06-13 08:57:38 -0400

    ..

--- TitleScreen.java
+++ TitleScreen.java
@@ -2,7 +2,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class TitleScreen extends World
 {
-    Label titleLabel = new Label("Capybara and Bird", 60);
+    Label titleLabel = new Label("Capybara and Birdie", 60);
 
     /**
      * Constructor for objects of class TitleScreen.
@@ -13,8 +13,8 @@ public class TitleScreen extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 600, 1);
         GreenfootImage titleBg = new GreenfootImage("images/titleBg.jpg");
-        getBackground().drawImage(titleBg, 275, 150);
-        addObject(titleLabel, 275, 200);
+        getBackground().drawImage(titleBg, 275, 200);
+        addObject(titleLabel, 285, 200);
         prepare();
     }
 
@@ -34,8 +34,7 @@ public class TitleScreen extends World
      */
     private void prepare()
     {
-        Label label = new Label("Press <space> to Start", 20);
-        addObject(label,91,325);
-        label.setLocation(138,350);
+        Label label = new Label("Press <space> to Start", 30);
+        addObject(label,190,280);
     }
 }
